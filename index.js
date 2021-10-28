8d87c5dd9da2b87d55d84b58cc9d8b1d9cdcbdc6
Wurielle <noahfenghom@gmail.com>
2021-10-28 16:11:40 +0200

    debugging

--- index.js
+++ index.js
@@ -2,35 +2,23 @@ const fs = require('fs');
 const path = require('path');
 
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
-    const version = core.getInput('version');
-    let targets = core.getInput('targets');
-    if (targets && typeof targets === 'string') targets = [targets];
+    let version = core.getInput('version');
+    if (version.startsWith('v')) version.substring(1);
 
-    console.log({
-        version,
-        targets,
-    });
-
-    if (targets && typeof targets === 'object' && Array.isArray(targets)) {
-        targets.forEach((packagePath) => {
-            console.log(process.env.GITHUB_WORKSPACE, packagePath);
-            const packageFilePath = path.join(process.env.GITHUB_WORKSPACE, packagePath);
-            const packageFileData = fs.readFileSync(packageFilePath);
-            const currentPackage = JSON.parse(packageFileData);
-            const newPackage = {
-                ...currentPackage,
-                version,
-            }
-            fs.writeFileSync(packageFilePath, JSON.stringify(newPackage, null, 2));
-            console.log(JSON.parse(fs.readFileSync(packageFilePath, null, 2)));
-        })
-    }
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    const targets = core.getMultilineInput('targets');
+    targets.forEach((packagePath) => {
+        const packageFilePath = path.join(process.env.GITHUB_WORKSPACE, packagePath);
+        const packageFileData = fs.readFileSync(packageFilePath);
+        const currentPackage = JSON.parse(packageFileData);
+        const newPackage = {
+            ...currentPackage,
+            version,
+        }
+        fs.writeFileSync(packageFilePath, JSON.stringify(newPackage, null, 2));
+        console.log(JSON.parse(fs.readFileSync(packageFilePath, null, 2)));
+    })
 } catch (error) {
     core.setFailed(error.message);
 }
