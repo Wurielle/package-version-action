a558c79e63be6f3b50851766c90ad52c3ec5e8d6
Wurielle <noahfenghom@gmail.com>
2021-10-28 16:40:20 +0200

    debugging

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ const core = require('@actions/core');
 try {
     let version = core.getInput('version');
     if (version.startsWith('v')) version = version.substring(1);
-
     const targets = core.getMultilineInput('targets');
-    targets.forEach((packagePath) => {
-        const packageFilePath = path.join(process.env.GITHUB_WORKSPACE, packagePath);
-        const packageFileData = fs.readFileSync(packageFilePath);
-        const currentPackage = JSON.parse(packageFileData);
-        const newPackage = {
-            ...currentPackage,
-            version,
-        }
-        fs.writeFileSync(packageFilePath, JSON.stringify(newPackage, null, 2));
-        console.log('------------------------');
-        console.log('Package: ', packageFilePath);
-        console.log(JSON.parse(fs.readFileSync(packageFilePath, null, 2)));
-        console.log('------------------------');
-    })
+    if (version && targets.length > 0) {
+        targets.forEach((packagePath) => {
+            const packageFilePath = path.join(process.env.GITHUB_WORKSPACE, packagePath);
+            const packageFileData = fs.readFileSync(packageFilePath);
+            const currentPackage = JSON.parse(packageFileData);
+            const newPackage = {
+                ...currentPackage,
+                version,
+            }
+            fs.writeFileSync(packageFilePath, JSON.stringify(newPackage, null, 2));
+            console.log('------------------------');
+            console.log('Package: ', packageFilePath);
+            console.log(JSON.parse(fs.readFileSync(packageFilePath, null, 2)));
+            console.log('------------------------');
+        })
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
